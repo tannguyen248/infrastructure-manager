e0c9fd86e887c6efe46d871185737cd7e1fa11e4
Hoa Dang <hoadv106@gmail.com>
2019-12-13 18:00:06 +0700

    add development configuration

--- app/config.js
+++ app/config.js
@@ -1,12 +1,24 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyBJZbv40pK9aC-Ri_lgdvzUiCVsVn5oQP4",
-  authDomain: "infrastructure-manager-5f00f.firebaseapp.com",
-  databaseURL: "https://infrastructure-manager-5f00f.firebaseio.com",
-  projectId: "infrastructure-manager-5f00f",
-  storageBucket: "infrastructure-manager-5f00f.appspot.com",
-  messagingSenderId: "947994363856",
-  appId: "1:947994363856:web:e37f9ebf6a375c7866dbd5"
-};
+const isProduction = false;
+
+export const firebaseConfig = isProduction ?
+  {
+    apiKey: "AIzaSyBJZbv40pK9aC-Ri_lgdvzUiCVsVn5oQP4",
+    authDomain: "infrastructure-manager-5f00f.firebaseapp.com",
+    databaseURL: "https://infrastructure-manager-5f00f.firebaseio.com",
+    projectId: "infrastructure-manager-5f00f",
+    storageBucket: "infrastructure-manager-5f00f.appspot.com",
+    messagingSenderId: "947994363856",
+    appId: "1:947994363856:web:e37f9ebf6a375c7866dbd5"
+  } : {
+    apiKey: "AIzaSyCs-2HFwKwJ1vIP7FJZIdvkO2l7PmPDGZU",
+    authDomain: "device-management-a36ee.firebaseapp.com",
+    databaseURL: "https://device-management-a36ee.firebaseio.com",
+    projectId: "device-management-a36ee",
+    storageBucket: "device-management-a36ee.appspot.com",
+    messagingSenderId: "20620421769",
+    appId: "1:20620421769:web:f9ec5b22b351cde116b458",
+    measurementId: "G-JJ81SZ45FS"
+  };
 
 
 export const androidClientId = '947994363856-hn1iqnn9on8h8dn7sgsj6j7ibc326uaf.apps.googleusercontent.com';
--- web/src/src/Firebase/firebase.js
+++ web/src/src/Firebase/firebase.js
@@ -2,15 +2,27 @@ import app from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyBJZbv40pK9aC-Ri_lgdvzUiCVsVn5oQP4',
-  authDomain: 'infrastructure-manager-5f00f.firebaseapp.com',
-  databaseURL: 'https://infrastructure-manager-5f00f.firebaseio.com',
-  projectId: 'infrastructure-manager-5f00f',
-  storageBucket: 'infrastructure-manager-5f00f.appspot.com',
-  messagingSenderId: '947994363856',
-  appId: '1:947994363856:web:e37f9ebf6a375c7866dbd5'
-};
+const isProduction = false;
+
+const firebaseConfig = isProduction ?
+  {
+    apiKey: "AIzaSyBJZbv40pK9aC-Ri_lgdvzUiCVsVn5oQP4",
+    authDomain: "infrastructure-manager-5f00f.firebaseapp.com",
+    databaseURL: "https://infrastructure-manager-5f00f.firebaseio.com",
+    projectId: "infrastructure-manager-5f00f",
+    storageBucket: "infrastructure-manager-5f00f.appspot.com",
+    messagingSenderId: "947994363856",
+    appId: "1:947994363856:web:e37f9ebf6a375c7866dbd5"
+  } : {
+    apiKey: "AIzaSyCs-2HFwKwJ1vIP7FJZIdvkO2l7PmPDGZU",
+    authDomain: "device-management-a36ee.firebaseapp.com",
+    databaseURL: "https://device-management-a36ee.firebaseio.com",
+    projectId: "device-management-a36ee",
+    storageBucket: "device-management-a36ee.appspot.com",
+    messagingSenderId: "20620421769",
+    appId: "1:20620421769:web:f9ec5b22b351cde116b458",
+    measurementId: "G-JJ81SZ45FS"
+  };
 
 class Firebase {
   constructor() {
